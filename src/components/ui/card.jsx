c3e9fb728a5d23739c1bb32b4b1bd03985aa23a9
Haroon Arshad <arshadharoon216@gmail.com>
2025-03-13 07:13:04 +0500

    fixed some error

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 import * as React from "react";
 
 const Card = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)} {...props} />
+  <div ref={ref} className={cn("rounded-lg bg-card text-card-foreground bg-white shadow-sm", className)} {...props} />
 ));
 Card.displayName = "Card";
 
