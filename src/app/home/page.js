8f540391ee6bdb86d163ba1ba453674b2085394f
Haroon Arshad <arshadharoon216@gmail.com>
2025-03-14 07:29:24 +0500

    the home page are completed.

---
+++ src/app/home/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import Header from '@/components/header/Header';
+import React from 'react'
+
+function page() {
+  return (
+    <>
+        <div>
+            <Header />
+        </div>
+    </>
+  )
+}
+
+export default page
