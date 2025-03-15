6ea3c7f3bc929e409625ebcc06e30cb3927748a2
Haroon Arshad <arshadharoon216@gmail.com>
2025-03-15 07:43:34 +0500

    created a hero section with full animation.

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,13 +1,21 @@
 "use client";
 
 import Header from '@/components/header/Header';
+import Hero from '@/components/hero-section/Hero';
 import React from 'react'
 
 function page() {
   return (
     <>
         <div>
+              {/* // Header */}
             <Header />
+
+              {/* // Hero Section */}
+            <Hero />
+
+              {/* // midSection */}
+            
         </div>
     </>
   )
