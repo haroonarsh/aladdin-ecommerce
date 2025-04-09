c520b21225bb3227ed03254b12b1197e338958ac
Haroon Arshad <arshadharoon216@gmail.com>
2025-04-10 00:18:28 +0500

    addded protection

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Page from "./home/page";
+import { ProtectedRoute } from "@/features/auth/ProtectedRoute";
 
 export default function Home() {
   return (
     <>
+      <ProtectedRoute>
       <Page />
+      </ProtectedRoute>
     </>
   );
 }
