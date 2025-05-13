076fdbe9b523db76c664103e17d8cfae55b6fe0a
Haroon Arshad <arshadharoon216@gmail.com>
2025-05-14 00:06:20 +0500

    refactor MidSection component for improved layout and responsiveness

--- src/components/midSection/MidSection.jsx
+++ src/components/midSection/MidSection.jsx
@@ -21,33 +21,33 @@ function MidSection() {
 
   return (
     <>
-        <div className='w-3/4  m-auto mt-20 mb-20'>
+        <div className='w-[94%] md:w-3/4  m-auto mt-20 mb-20'>
             <div className='flex justify-between items-center'>
-                <h1 className='text-3xl font-bold'>Explore popular category</h1>
+                <h1 className='text-xl sm:text-2xl md:text-3xl font-bold'>Explore popular category</h1>
                 <div onClick={handleToken} className='flex items-center gap-2 cursor-pointer text-xl'>
-                <h2 onClick={() => router.push("/products-page")} className='flex items-center gap-2 cursor-pointer text-xl'>| See all<FaAngleRight /></h2>
+                <h2 onClick={() => router.push("/products-page")} className='flex items-center gap-2 cursor-pointer text-[16px] md:text-xl'>| See all<FaAngleRight /></h2>
                 </div>
             </div>
-                {/* // Categories items */}
-            <div className='flex justify-between gap-6 items-center mt-10'>
-              <div className='flex flex-col items-center cursor-pointer'>
-                <img className='w-5/6' src="./categories-images/img-1.png" alt="Category image" />
+                {/* // Categori es items */}
+            <div className='flex flex-wrap justify-between gap-6 items-center mt-10'>
+              <div className='flex sm:w-44 flex-col items-center cursor-pointer'>
+                <img className='w-5/6 rounded-lg' src="./categories-images/img-1.png" alt="Category image" />
                 <h1 className='mt-2 font-sans'>Beauty & Personal Care</h1>
               </div>
-              <div className='flex flex-col items-center cursor-pointer'>
-                <img className='w-5/6' src="./categories-images/img-2.png" alt="Category image" />
+              <div className='flex sm:w-44 flex-col items-center cursor-pointer'>
+                <img className='w-5/6 rounded-lg' src="./categories-images/img-2.png" alt="Category image" />
                 <p className='mt-2 font-sans'>Health & Household</p>
               </div>
-              <div className='flex flex-col items-center cursor-pointer'>
-                <img className='w-5/6' src="./categories-images/img-3.png" alt="Category image" />
+              <div className='flex sm:w-44 flex-col items-center cursor-pointer'>
+                <img className='w-5/6 rounded-lg' src="./categories-images/img-3.png" alt="Category image" />
                 <p className='mt-2 font-sans'>Home & Kitchen</p>
               </div>
-              <div className='flex flex-col items-center cursor-pointer'>
-                <img className='w-5/6' src="./categories-images/img-4.png" alt="Category image" />
+              <div className='flex sm:w-44 flex-col items-center cursor-pointer'>
+                <img className='w-5/6 rounded-lg' src="./categories-images/img-4.png" alt="Category image" />
                 <p className='mt-2 font-sans'>Aladdin Pharmacy</p>
               </div>
-              <div className='flex flex-col items-center cursor-pointer'>
-                <img className='w-5/6' src="./categories-images/img-5.png" alt="Category image" />
+              <div className='flex sm:w-44 flex-col items-center cursor-pointer'>
+                <img className='w-5/6 rounded-lg' src="./categories-images/img-5.png" alt="Category image" />
                 <p className='mt-2 font-sans'>Medical Instruments</p>
               </div>
             </div>
@@ -55,9 +55,9 @@ function MidSection() {
                 {/* // Brands */}
             <div className='mt-14'>
               <div className='flex justify-between items-center'>
-                <h1 className='text-3xl font-bold'>Popular brands</h1>
+                <h1 className='text-xl sm:text-2xl md:text-3xl font-bold'>Popular brands</h1>
               </div>
-              <div className='flex gap-6 items-center mt-6'>
+              <div className='flex flex-wrap justify-center sm:justify-start gap-6 items-center mt-6'>
                 <img className='w-20' src="./brand-icons/Vector.png" alt="Vector" />
                 <img className='w-20' src="./brand-icons/Dove.png" alt="Dove" />
                 <img className='w-20' src="./brand-icons/Group.png" alt="Heart" />
