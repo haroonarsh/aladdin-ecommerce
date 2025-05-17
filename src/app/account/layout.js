ef6882bbdad498d9a7f39aa80f2fbed103b9ae41
Haroon Arshad <arshadharoon216@gmail.com>
2025-05-17 22:38:51 +0500

    refactor layout and styling in Account and Wallet pages for improved responsiveness and visual consistency

--- src/app/account/layout.js
+++ src/app/account/layout.js
@@ -11,7 +11,7 @@ export default async function AccountLayout({ children }) {
         <div className='max-w-[1300px] mx-auto flex lg:mt-[6.75rem] mt-[4.25rem] mb-6 font-sans'>
             <ProfileSideBar />
             <main className='w-5/8'>
-              <div className=" md:pl-8">
+              <div className="md:pl-8">
                 { children }
               </div>
             </main>
--- src/app/account/wallet/page.js
+++ src/app/account/wallet/page.js
@@ -6,15 +6,15 @@ import React from 'react'
 function page() {
   return (
     <>
-        {/* <div className="pl-6 md:pl-8"> */}
-            <h2 className="mb-6 text-2xl font-semibold text-cyan-800">Wallet Information</h2>
+        <div className="sm:pl-4 p-1.5 sm:pr-4  pr-1.5 md:pl-0">
+            <h2 className="mb-6 md:text-2xl text-xl font-semibold text-cyan-800">Wallet Information</h2>
                 <WalletBalance balance={4500} />
                 <PaymentMethods />
 
                 <div className="mt-8 mb-4">
                     <TransactionHistory />
                 </div>
-      {/* </div> */}
+      </div>
     </>
   )
 }
