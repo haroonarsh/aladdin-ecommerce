8a98f205a26985ae38278d63e8b83f2aca54c757
Haroon Arshad <arshadharoon216@gmail.com>
2025-05-27 11:46:39 +0500

    update recharts version to 2.15.3; add DashboardLayout component and implement token retrieval in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import { cookies } from "next/headers";
 import Page from "./home/page";
 
-export default function Home() {
+export default async function Home() {
+
+    const token = await cookies().get("jwt")?.value;
+    console.log("HomeToken:", token); // Log the token value
   return (
     <>
-      <Page />
+      <Page token={token} />
     </>
   );
 }
