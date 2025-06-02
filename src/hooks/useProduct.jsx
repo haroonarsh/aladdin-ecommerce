3da15fbc5bf3f493752b197598a38d3c8baf24f6
Haroon Arshad <arshadharoon216@gmail.com>
2025-06-02 13:05:03 +0500

    Add success and error toast notifications for product creation, update, and deletion

--- src/hooks/useProduct.jsx
+++ src/hooks/useProduct.jsx
@@ -1,3 +1,5 @@
+import { toast } from "react-toastify";
+
 const { productService } = require("@/services/product.service");
 
 export const useProduct = () => {
@@ -25,6 +27,7 @@ export const useProduct = () => {
     const createProduct = async (token, formData) => {
         try {
             const product = await productService.createProduct(token, formData);
+            toast.success("Product created successfully!");
             return product;
         } catch (error) {
             console.error("Error creating product:", error);
@@ -35,16 +38,18 @@ export const useProduct = () => {
     const updateProduct = async (token, id, formData) => {
         try {
             const product = await productService.updateProduct(token, id, formData);
+            toast.success("Product updated successfully!");
             return product;
         } catch (error) {
-            console.error("Error updating product:", error);
-            throw error; // Re-throw the error for further handling
+            toast.error("Error updating product: " + error.response?.data?.message || "Please try again.");
+            console.log("Error updating product:", error);
         }
     }
 
     const deleteProduct = async (token, id) => {
         try {
             const response = await productService.deleteProduct(token, id);
+            toast.success("Product deleted successfully!");
             return response;
         } catch (error) {
             console.error("Error deleting product:", error);
