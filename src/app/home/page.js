d6b9f0fd5a52e5eadd07463fb17d748f74e91e09
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-10 00:36:27 +0500

    Implement admin token validation and redirect logic in page component

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,9 +3,31 @@
 import Hero from '@/components/hero-section/Hero';
 import MidSection from '@/components/midSection/MidSection';
 import React from 'react'
+import { useUser } from "@/hooks/useUser";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 function page({ token }) {
   console.log("Page Token:", token); // Log the token value
+
+  
+    const { fetchAdmin } = useUser();
+    const router = useRouter();
+  
+    useEffect(() => {
+      if (token) {
+        fetchAdmin().then((data) => {
+          if (data?.user?.Role === "admin") { 
+            router.push("/admin/dashboard");
+          }
+        }).catch((error) => {
+          console.error("Error fetching admin data:", error);
+          router.push("/login");
+        });
+      } else {
+        router.push("/login");
+      }
+    }, [token, fetchAdmin, router]);
   return (
     <>
           <div>
