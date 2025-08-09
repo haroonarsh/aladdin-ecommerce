f71d58d58b9a91d8efa8a501ef9387a46fc2b5e4
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-10 00:37:58 +0500

    Add checkAdmin function to fetch user data and handle admin redirection

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,5 @@
-import jwt, { decode } from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
+import 'dotenv/config'; // Ensure environment variables are loaded
 
 export const verifyJWT = (token) => {
     return new Promise((resolve, reject) => {
